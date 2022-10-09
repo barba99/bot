2c665eb9c8fd22320bbf197b953fe5341421b200
Jorge Barba <olegariobarba@gmail.com>
2022-10-09 10:51:12 -0500

    Delete configs.py

--- configs.py
+++
@@ -1,9 +0,0 @@
-from os import path, getenv
-
-class Config:
-    API_ID = int(getenv("API_ID", "808192"))
-    API_HASH = getenv("API_HASH", "d2a1c1fe0d574f98068c63d4928aee8a")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1819174855:AAFdKupTR5oUEM6aitsduq8TVyLIBaa4lVs")
-
-config = Config()
-
